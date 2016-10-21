9a1461dd04f98b88cb3ce1745538f2d8b9c53678
yanlibo <yaniibo2010@163.com>
2016-10-21 10:58:07 +0800

    vuejs vuex ajax

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-/*
- * @Author: henry yan
- * @Date:   2016-10-19 16:47:00
- * @Last Modified by:   henry yan
- * @Last Modified time: 2016-10-19 16:47:00
- */
+'use strict'
+
+// -- api server
+
+var http = require("http");
+var Api = require("./server_api_define.js").Api;
+
+var api_root_dir = __dirname + "../fufang_api";;
+console.log("root_dir : " + api_root_dir)
+
+var api = new Api();
+var server = http.createServer();
+server.on("request", function(request, response) {
+    api.dispatch(request, response, api_root_dir);
+});
+server.listen(3001);
+
+console.log("-- file api server is running at 3001 port.\n");
+
 // -- react hot replace
 
 var webpack = require('webpack');
@@ -12,14 +25,25 @@ var config = require('./webpack.config.js');
 config.entry.unshift('webpack-dev-server/client?http://localhost:4000', "webpack/hot/dev-server");
 config.plugins.push(new webpack.HotModuleReplacementPlugin());
 
+// 这里配置：请求http://localhost:8080/api，
+// 相当于通过本地node服务代理请求后台服务器了http://cn.vuejs.org/api
+var proxy = [{
+    path: '/qm/*',
+    target: 'http://localhost:3001',
+    secure: false
+}]
 //启动服务
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot:true,
     historyApiFallback: true,
-    stats: { colors: true},
+    stats: { colors: true},  //打包输出时带颜色
     proxy: {
         '/api/*': {
+            target: 'http://localhost:3001',
+            secure: false
+        },
+        '/qm/*': {
             target: 'http://localhost:3001',
             secure: false
         }
@@ -28,5 +52,5 @@ new WebpackDevServer(webpack(config), {
     if (err) {
         console.log(err);
     };
-    console.log('-- html dev server running at localhost:4000');
+    console.log('-- html dev server running at localhost:3000');
 });
